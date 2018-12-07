e4a571ddc00e7d489a8edca5a81902ec19ca95b9
divyapuja <divyapuja.vitonde@gmail.com>
2018-12-06 22:25:31 -0500

    modified little

--- initial.sh
+++ initial.sh
@@ -5,9 +5,20 @@ sudo apt-get update
 sudo apt-get install -y ansible
 sudo apt install -y docker.io
 sudo apt-get install python -y
-sudo sudo docker run -itd --privileged --name=PreSoupImage --hostname=PreSoupImage ubuntu
+sudo docker run -itd --privileged --name=PreSoupImage --hostname=PreSoupImage ubuntu
 sh installationForSoup.sh PreSoupImage
-sudo docker run -itd --privileged --name=PreSoupDNS --hostname=PreSoupDNS ubuntu
+sudo docker run -itd --privileged --name=PreSoupDNS --hostname=PreSoupDNS divyapuja/subnetdns
 sh installationForDNS.sh PreSoupDNS
+sudo docker run -itd --privileged --name=SOUPVPCDNS --hostname=SOUPVPCDNS divyapuja/vpcdns
+sh installationForDNS.sh SOUPVPCDNS
+sudo docker run -itd --privileged --name=SOUPCentralDNS --hostname=SOUPCeentralDNS divyapuja/centraldns
+sh installationForDNS.sh SOUPCentralDNS
 sudo docker commit PreSoupImage soupimage
-sudo docker commit PreSoupDNS soupdns
+sudo docker commit PreSoupDNS soupsubnetdns
+sudo docker commmit SOUPVPCDNS soupvpcdns
+sudo docker commit SOUPCentralDNS soupcentraldns
+sudo docker stop PreSoupImage
+sudo docker stop PreSoupDNS
+sudo docker stop SOUPVPCDNS
+sudo docker stop SOUPCentralDNS
+sudo docker system prune
