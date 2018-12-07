0941cd66522b59c8ce07a408d022a816b36e7a4f
Divyapuja <divyapuja.vitonde@gmail.com>
2018-12-07 12:17:35 -0500

    Update installationForDNS.sh

--- installationForDNS.sh
+++ installationForDNS.sh
@@ -5,4 +5,4 @@ sudo docker exec -i $1 apt-get install tcpdump -y
 sudo docker exec -i $1 apt-get install net-tools -y
 sudo docker exec -i $1 apt-get install iproute2 -y
 sudo docker exec -i $1 apt install iputils-ping -y
-sudo apt-get install bind9 bind9utils bind9-doc -y
+sudo docker exec -i $1 apt-get install bind9 bind9utils bind9-doc -y
