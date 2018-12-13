f93ce7fbe12b3a8b8c3a12d41803b5a3745e45ae
Divyapuja <divyapuja.vitonde@gmail.com>
2018-12-12 23:25:49 -0500

    Update initial.sh
    docker images changed

--- initial.sh
+++ initial.sh
@@ -5,6 +5,33 @@ sudo apt-get update
 sudo apt-get install -y ansible
 sudo apt install -y docker.io
 sudo apt-get install python -y
+sudo docker run -itd --privileged --name=PreSoupImage --hostname=PreSoupImage divyapuja/host
+#sh installationForSoup.sh PreSoupImage
+sudo docker run -itd --privileged --name=PreSoupDNS --hostname=PreSoupDNS divyapuja/subnetdns1
+#sh installationForDNS.sh PreSoupDNS
+sudo docker run -itd --privileged --name=SoupVPCDNS --hostname=SoupVPCDNS divyapuja/vpcdnsdns1
+#sh installationForDNS.sh SoupVPCDNS
+sudo docker run -itd --privileged --name=SoupCentralDNS --hostname=SoupCeentralDNS divyapuja/centraldns
+#sh installationForDNS.sh SoupCentralDNS
+sudo docker commit PreSoupImage soupimage
+sudo docker commit PreSoupDNS soupsubnetdns
+sudo docker commmit SoupVPCDNS soupvpcdns
+sudo docker commit SoupCentralDNS soupcentraldns
+sudo docker stop PreSoupImage
+sudo docker stop PreSoupDNS
+sudo docker stop SoupVPCDNS
+sudo docker stop SoupCentralDNS
+sudo docker system prune
+
+
+#-------------------------------------------------------------------------------------------------------#
+#!/bin/bash
+
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
+sudo apt-get install -y ansible
+sudo apt install -y docker.io
+sudo apt-get install python -y
 sudo docker run -itd --privileged --name=PreSoupImage --hostname=PreSoupImage ubuntu
 sh installationForSoup.sh PreSoupImage
 sudo docker run -itd --privileged --name=PreSoupDNS --hostname=PreSoupDNS divyapuja/subnetdns
