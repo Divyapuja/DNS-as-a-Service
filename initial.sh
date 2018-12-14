80a1a6557e9f0d031c77fa6b51f8c18bc636f159
Divyapuja <divyapuja.vitonde@gmail.com>
2018-12-13 22:35:42 -0500

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -25,27 +25,27 @@ sudo docker system prune
 
 
 #-------------------------------------------------------------------------------------------------------#
-#!/bin/bash
+#-------------!/bin/bash
 
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt-get update
-sudo apt-get install -y ansible
-sudo apt install -y docker.io
-sudo apt-get install python -y
-sudo docker run -itd --privileged --name=PreSoupImage --hostname=PreSoupImage ubuntu
-sh installationForSoup.sh PreSoupImage
-sudo docker run -itd --privileged --name=PreSoupDNS --hostname=PreSoupDNS divyapuja/subnetdns
-sh installationForDNS.sh PreSoupDNS
-sudo docker run -itd --privileged --name=SOUPVPCDNS --hostname=SOUPVPCDNS divyapuja/vpcdns
-sh installationForDNS.sh SOUPVPCDNS
-sudo docker run -itd --privileged --name=SOUPCentralDNS --hostname=SOUPCeentralDNS divyapuja/centraldns
-sh installationForDNS.sh SOUPCentralDNS
-sudo docker commit PreSoupImage soupimage
-sudo docker commit PreSoupDNS soupsubnetdns
-sudo docker commmit SOUPVPCDNS soupvpcdns
-sudo docker commit SOUPCentralDNS soupcentraldns
-sudo docker stop PreSoupImage
-sudo docker stop PreSoupDNS
-sudo docker stop SOUPVPCDNS
-sudo docker stop SOUPCentralDNS
-sudo docker system prune
+#sudo apt-add-repository ppa:ansible/ansible -y
+#sudo apt-get update
+#sudo apt-get install -y ansible
+#sudo apt install -y docker.io
+#sudo apt-get install python -y
+#sudo docker run -itd --privileged --name=PreSoupImage --hostname=PreSoupImage ubuntu
+#sh installationForSoup.sh PreSoupImage
+#sudo docker run -itd --privileged --name=PreSoupDNS --hostname=PreSoupDNS divyapuja/subnetdns
+#sh installationForDNS.sh PreSoupDNS
+#sudo docker run -itd --privileged --name=SOUPVPCDNS --hostname=SOUPVPCDNS divyapuja/vpcdns
+#sh installationForDNS.sh SOUPVPCDNS
+#sudo docker run -itd --privileged --name=SOUPCentralDNS --hostname=SOUPCeentralDNS divyapuja/centraldns
+#sh installationForDNS.sh SOUPCentralDNS
+#sudo docker commit PreSoupImage soupimage
+#sudo docker commit PreSoupDNS soupsubnetdns
+#sudo docker commmit SOUPVPCDNS soupvpcdns
+#sudo docker commit SOUPCentralDNS soupcentraldns
+#sudo docker stop PreSoupImage
+#sudo docker stop PreSoupDNS
+#sudo docker stop SOUPVPCDNS
+#sudo docker stop SOUPCentralDNS
+#sudo docker system prune
