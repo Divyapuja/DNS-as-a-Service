df4b1e00890d78cef759b25dbae132aa2eefe7a7
Divyapuja <divyapuja.vitonde@gmail.com>
2018-12-15 09:38:17 -0500

    Update installationForDNS.sh
    DNS container should have ansible installed so that it can run chDNSscript.yml by default

--- installationForDNS.sh
+++ installationForDNS.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo docker exec -i $1 apt-get update
 sudo docker exec -i $1 apt-get upgrade -y
-sudo docker exec -i $1 apt-get install tcpdump -y
-sudo docker exec -i $1 apt-get install net-tools -y
-sudo docker exec -i $1 apt-get install iproute2 -y
-sudo docker exec -i $1 apt install iputils-ping -y
-sudo docker exec -i $1 apt-get install bind9 bind9utils bind9-doc -y
+sudo docker exec -i $1 apt-add-repository ppa:ansible/ansible -y
+sudo docker exec -i $1 apt-get update
+sudo docker exec -i $1 apt-get install -y ansible
+sudo docker exec -i $1 apt-get install python -y
+#need to copy ansible script as well
