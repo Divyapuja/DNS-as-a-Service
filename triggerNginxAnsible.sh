b90e31f25f5f68d5139b35bf4f5b875578413610
Divyapuja <divyapuja.vitonde@gmail.com>
2018-12-16 14:37:16 -0500

    Update triggerNginxAnsible.sh

--- triggerNginxAnsible.sh
+++ triggerNginxAnsible.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # remove /24 from $2
-Subnet_IP=$1
-
-export Subnet_IP
+centralDNS1_IP=$1
+centralDNS2_IP=$2
+export centralDNS1_IP
+export centralDNS2_IP
 
 ip route del default
-ip route add default via $VPC_IP 
+ip route add default via 50.50.50.1 
 ansible-playbook -i inventory main.yml
