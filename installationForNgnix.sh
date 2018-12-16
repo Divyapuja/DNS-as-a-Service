4115af900ea89f2963e9771b5fbf26ab58b1cd6a
Divyapuja <divyapuja.vitonde@gmail.com>
2018-12-16 13:38:19 -0500

    Update installationForNgnix.sh

--- installationForNgnix.sh
+++ installationForNgnix.sh
@@ -3,3 +3,5 @@ sudo docker exec -i $1 iptables -I INPUT -p udp --dport 53 -m state --state NEW 
 sudo docker exec -i $1 iptables -I INPUT -p udp --dport 53 -m state --state NEW -m recent --update --seconds 2 --hitcount 10 -j DROP
 sudo docker cp triggerNginxAnsible.sh $1:/triggerNginxAnsible.sh
 sudo docker cp chNginxscript.yml $1:/main.yml
+
+
