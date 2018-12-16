fd47a318d1aa75448d5970394524bf338d215cc0
Divyapuja <divyapuja.vitonde@gmail.com>
2018-12-16 13:26:05 -0500

    Update installationForDNS.sh

--- installationForDNS.sh
+++ installationForDNS.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-sudo docker exec -i $1 apt-get update
-sudo docker exec -i $1 apt-get upgrade -y
-sudo docker exec -i $1 apt-add-repository ppa:ansible/ansible -y
-sudo docker exec -i $1 apt-get update
-sudo docker exec -i $1 apt-get install -y ansible
-sudo docker exec -i $1 apt-get install python -y
+sudo docker exec -i $1 yum install epel-release
+sudo docker exec -i $1 yum install ansible
+sudo docker exec -i $1 yum install python -y
 sudo docker cp triggerDNSAnsible.sh $1:/triggerDNSAnsible.sh
 sudo docker cp chDNSscript.yml $1:/main.yml
 
